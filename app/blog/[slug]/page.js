ae225e82fd3ac10e40c1b91f8f1fbe9fe101641b
Shinsieon <coolguysiun@gmail.com>
2024-01-09 23:34:28 +0900

    blog

--- app/blog/[slug]/page.js
+++ app/blog/[slug]/page.js
@@ -4,19 +4,32 @@ import ReactMarkdown from "react-markdown";
 import remarkGfm from "remark-gfm";
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
 import { materialDark } from "react-syntax-highlighter/dist/cjs/styles/prism";
+import matter from "gray-matter";
+import Image from "next/image";
 
 const postsDirectory = join(process.cwd(), "app/blog/posts");
 
 export default async function Page({ params }) {
-  const postData = await getPostData(params.slug);
+  console.log(params);
+  const { title, date, content } = await getPostData(params.slug);
   return (
     <article className="flex min-h-screen flex-col bg-[#121212] px-12  ">
       <div className="container mx-auto px-12 py-12 text-white  font-ios">
+        <h2 className="text-4xl font-extrabold dark:text-white mb-2">
+          {title}
+        </h2>
+        <p className="text-sm font-normal text-gray-500 dark:text-gray-400 mb-5">
+          {date}
+        </p>
         <ReactMarkdown
           remarkPlugins={[remarkGfm]} // Allows us to have embedded HTML tags in our markdown
           components={{
             h1(props) {
-              return <h1 className="text-3xl py-2">{props.children}</h1>;
+              return (
+                <h1 className="bg-blue-100 text-blue-800 text-2xl font-semibold px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 mb-5 inline-block">
+                  {props.children}
+                </h1>
+              );
             },
             blockquote({ children, ...props }) {
               return (
@@ -33,7 +46,11 @@ export default async function Page({ params }) {
               );
             },
             p(props) {
-              return <p className="font-sans">{props.children}</p>;
+              return (
+                <p className="mb-3 text-gray-100 dark:text-warmGray-50">
+                  {props.children}
+                </p>
+              );
             },
 
             code({ node, inline, className, children, ...props }) {
@@ -57,12 +74,11 @@ export default async function Page({ params }) {
                 alt={image.alt || ""}
                 width={500}
                 height={300}
-                className={classes.markdown_container_img}
               />
             ),
           }}
         >
-          {postData}
+          {content}
         </ReactMarkdown>
       </div>
     </article>
@@ -77,6 +93,11 @@ export async function generateStaticParams() {
 export async function getPostData(id) {
   const fullPath = join(postsDirectory, `${id}.md`);
   const fileContents = fs.readFileSync(fullPath, "utf8");
-
-  return fileContents;
+  const { data, content, desc } = matter(fileContents);
+  console.log(desc);
+  return {
+    title: data.title,
+    date: data.date,
+    content,
+  };
 }
