8acd59aacf3e74dbd652d3b33d4136aad77234bf
Shinsieon <coolguysiun@gmail.com>
2024-01-21 22:22:51 +0900

    chart

--- app/components/AboutSection.jsx
+++ app/components/AboutSection.jsx
@@ -5,10 +5,10 @@ import { Chart, ArcElement } from "chart.js";
 Chart.register(ArcElement);
 
 const data = {
-  labels: ["Red", "Green", "Yellow"],
+  labels: ["Javascript", "Typescript", "Python"],
   datasets: [
     {
-      data: [300, 50, 100],
+      data: [50, 25, 25],
       backgroundColor: ["#FF6384", "#36A2EB", "#FFCE56"],
       hoverBackgroundColor: ["#FF6384", "#36A2EB", "#FFCE56"],
     },
@@ -19,13 +19,10 @@ const TAB_DATA = [
     title: "Skills",
     id: "skills",
     component: (
-      <div
-        className="p-3 rounded-lg bg-gray-50  dark:bg-gray-800"
-        role="tabpanel"
-      >
-        <h2>Doughnut Example</h2>
-        <div className="w-[100px]">
-          <Doughnut data={data} width={50} height={50} />
+      <div className="p-3 rounded-lg" role="tabpanel">
+        <h2>Skills I have</h2>
+        <div className="w-[300px] h-[300px]">
+          <Doughnut data={data} width={300} height={300} />
         </div>
       </div>
     ),
@@ -87,3 +84,86 @@ const AboutSection = () => {
 };
 
 export default AboutSection;
+
+// import React, { useEffect, useState } from "react";
+// import {
+//     Chart as ChartJS,
+//     CategoryScale,
+//     LinearScale,
+//     PointElement,
+//     LineElement,
+//     Title,
+//     Tooltip,
+//     Legend,
+// } from "chart.js";
+// import { Line } from "react-chartjs-2";
+// import { useRecoilState } from "recoil";
+// import { urlState } from "../../src/recoilState";
+
+// ChartJS.register(
+//     CategoryScale,
+//     LinearScale,
+//     PointElement,
+//     LineElement,
+//     Title,
+//     Tooltip,
+//     Legend
+// );
+
+// function Lastweek() {
+//     const [rootUrl, setRootUrl] = useRecoilState(urlState);
+//     const [chatData, setChatData] = useState([]);
+
+//     useEffect(() => {
+//         const fetchData = async () => {
+//             try {
+//                 const response = await fetch(
+//                     `${rootUrl}/admin/dashboard/lastweek`
+//                 );
+//                 const json = await response.json();
+//                 setChatData(json.data);
+//             } catch (error) {
+//                 console.log("Error :", error);
+//             }
+//         };
+//         fetchData();
+//         console.log(chatData);
+//     }, []);
+
+//     const options = {
+//         responsive: true,
+//         plugins: {
+//             legend: {
+//                 position: "top",
+//             },
+//             title: {
+//                 display: true,
+//                 text: "지난 7일 간 채팅 건수",
+//             },
+//         },
+//     };
+
+//     let labels = [];
+//     if (chatData.length > 0) {
+//         labels = chatData.map((data) => data.x);
+//     }
+
+//     const data = {
+//         labels,
+//         datasets: [
+//             {
+//                 label: "채팅 건수",
+//                 data: chatData.map((data) => data.y),
+//                 borderColor: "rgb(255, 99, 132)",
+//                 backgroundColor: "rgba(255, 99, 132, 0.5)",
+//             },
+//         ],
+//     };
+//     return (
+//         <div>
+//             {chatData.length > 0 && <Line options={options} data={data} />}
+//         </div>
+//     );
+// }
+
+// export default Lastweek;
