7a97853bcb347e16d85920260d16a1f7dd76a131
Shinsieon <coolguysiun@gmail.com>
2024-02-03 17:56:04 +0900

    pdf change

--- app/components/HeroSection.jsx
+++ app/components/HeroSection.jsx
@@ -24,7 +24,7 @@ const HeroSection = () => {
             lines of code, I can make a better world.
           </p>
           <Link
-            href="/files/신시언 이력서.pdf"
+            href="/files/신시언 이력서2.pdf"
             className="text-gray-900 bg-white hover:bg-gray-100 border border-gray-200 focus:ring-4 focus:outline-none text-nowrap focus:ring-gray-100 font-bold rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-gray-600 dark:bg-gray-800 dark:border-gray-700 dark:text-white dark:hover:bg-gray-700 me-2 mb-2"
           >
             <DocumentArrowDownIcon className="w-5 h-5" />
