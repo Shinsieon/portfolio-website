31ddaa77d4afebf88d20831b4765a73132986563
Shinsieon <coolguysiun@gmail.com>
2024-02-07 22:45:19 +0900

    blog

--- app/components/BlogTableSection.jsx
+++ app/components/BlogTableSection.jsx
@@ -1,45 +1,9 @@
 "use client";
 import React, { useState, useEffect } from "react";
-import {
-  MagnifyingGlassIcon,
-  ChevronUpDownIcon,
-} from "@heroicons/react/24/outline";
-import {
-  Card,
-  CardHeader,
-  Input,
-  Typography,
-  Button,
-  CardBody,
-  Chip,
-  CardFooter,
-  Tabs,
-  TabsHeader,
-  Tab,
-  Avatar,
-} from "@material-tailwind/react";
+import { Typography, Avatar } from "@material-tailwind/react";
 import Link from "next/link";
 
-const TABS = [
-  {
-    label: "All",
-    value: "all",
-  },
-  {
-    label: "Algorithms",
-    value: "Algorithms",
-  },
-  {
-    label: "Javascript",
-    value: "Javascript",
-  },
-  {
-    label: "Python",
-    value: "Python",
-  },
-];
-
-const TABLE_HEAD = ["Title", "Skills", "Created", "View"];
+const TABLE_HEAD = ["Title", "Skills", "Created", "Viewed"];
 const options = {
   method: "GET",
   headers: {
@@ -63,81 +27,74 @@ const BlogTableSection = () => {
       <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
         <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
           <tr>
-            <th scope="col" className="px-6 py-3">
-              Product name
-            </th>
-            <th scope="col" className="px-6 py-3">
-              Color
-            </th>
-            <th scope="col" className="px-6 py-3">
-              Category
-            </th>
-            <th scope="col" className="px-6 py-3">
-              Price
-            </th>
-            <th scope="col" className="px-6 py-3">
-              Action
-            </th>
+            {TABLE_HEAD.map((head, index) => (
+              <th key={index} className="cursor-pointer px-6 py-3 text-left">
+                {head}
+              </th>
+            ))}
           </tr>
         </thead>
         <tbody>
-          <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-            <th
-              scope="row"
-              className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-            >
-              Apple MacBook Pro 17"
-            </th>
-            <td className="px-6 py-4">Silver</td>
-            <td className="px-6 py-4">Laptop</td>
-            <td className="px-6 py-4">$2999</td>
-            <td className="px-6 py-4">
-              <a
-                href="#"
-                className="font-medium text-blue-600 dark:text-blue-500 hover:underline"
-              >
-                Edit
-              </a>
-            </td>
-          </tr>
-          <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-            <th
-              scope="row"
-              className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-            >
-              Microsoft Surface Pro
-            </th>
-            <td className="px-6 py-4">White</td>
-            <td className="px-6 py-4">Laptop PC</td>
-            <td className="px-6 py-4">$1999</td>
-            <td className="px-6 py-4">
-              <a
-                href="#"
-                className="font-medium text-blue-600 dark:text-blue-500 hover:underline"
-              >
-                Edit
-              </a>
-            </td>
-          </tr>
-          <tr className="bg-white dark:bg-gray-800">
-            <th
-              scope="row"
-              className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-            >
-              Magic Mouse 2
-            </th>
-            <td className="px-6 py-4">Black</td>
-            <td className="px-6 py-4">Accessories</td>
-            <td className="px-6 py-4">$99</td>
-            <td className="px-6 py-4">
-              <a
-                href="#"
-                className="font-medium text-blue-600 dark:text-blue-500 hover:underline"
-              >
-                Edit
-              </a>
-            </td>
-          </tr>
+          {postArr.length > 0 &&
+            postArr.map(({ fileInfo, fileName }, index) => {
+              const { title, date, skills, cover_image, viewed, content } =
+                fileInfo;
+              return (
+                <tr
+                  className="bg-white border-b dark:bg-gray-800 dark:border-gray-700"
+                  key={index}
+                >
+                  <th
+                    scope="row"
+                    class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                  >
+                    <div className="flex items-center gap-3">
+                      <Avatar
+                        src={`/images/${cover_image}`}
+                        alt={name}
+                        size="sm"
+                      />
+                      <div className="flex flex-col">
+                        <Link
+                          variant="small"
+                          className="font-normal"
+                          href={`/blog/${fileName.replace(".md", "")}`}
+                        >
+                          {title}
+                        </Link>
+                        <Typography
+                          variant="small"
+                          className="font-normal opacity-70"
+                        >
+                          {""}
+                        </Typography>
+                      </div>
+                    </div>
+                  </th>
+                  <td className="p-4">
+                    <div className="flex flex-col">
+                      <Typography variant="small" className="font-normal">
+                        {skills}
+                      </Typography>
+                    </div>
+                  </td>
+
+                  <td className="p-4">
+                    <Typography variant="small" className="font-normal">
+                      {date}
+                    </Typography>
+                  </td>
+                  <td className="p-4">
+                    <Typography
+                      variant="small"
+                      className="font-normal text-center"
+                    >
+                      {viewed}
+                    </Typography>
+                  </td>
+                </tr>
+              );
+            })}
         </tbody>
       </table>
     </div>
