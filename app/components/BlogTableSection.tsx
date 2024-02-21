bcd0eebf716ee34229bc904dfc04ea00a1cac5f4
Shinsieon <coolguysiun@gmail.com>
2024-02-21 21:04:33 +0900

    fix

--- app/components/BlogTableSection.tsx
+++ app/components/BlogTableSection.tsx
@@ -115,7 +115,7 @@ const BlogTableSection = () => {
             currentPage
           ).map((_, index) =>
             currentPage === index + 1 ? (
-              <li>
+              <li key={index}>
                 <a
                   href="#"
                   className="flex items-center justify-center px-4 w-12 h-14 leading-tight text-blue-600 border border-gray-300 bg-blue-50 hover:bg-blue-100 hover:text-blue-700 dark:border-gray-700 dark:bg-gray-700 dark:text-white"
@@ -125,7 +125,7 @@ const BlogTableSection = () => {
                 </a>
               </li>
             ) : (
-              <li>
+              <li key={index}>
                 <a
                   href="#"
                   className="flex items-center justify-center px-3 w-12 h-14 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
